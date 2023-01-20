6017235650167de93f719b7103c33e561b1635c0
Md. Harun OR Rashid <108585636+harunrashid4419@users.noreply.github.com>
2023-01-20 16:11:45 +0600

    code change

--- index.js
+++ index.js
@@ -14,16 +14,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-<<<<<<< HEAD
-    try{
-        
-        const usersCollections = client.db('easy-doc').collection('users');
-        const tutorialCollections = client.db('easy-doc').collection('tutorial');
-        const categoriesCollections = client.db('easy-doc').collection('categories');
-        
-        // insert users with email password
-        app.post('/users', async(req, res) =>{
-=======
     try {
 
         const userCollections = client.db('easy-doc').collection('users');
@@ -32,10 +22,8 @@ async function run() {
         // when user register he/she will be inserted in userCollection
         // if user already exist nothing changes  happened
         app.put('/user', async (req, res) => {
->>>>>>> main
             const user = req.body;
             const uid = req?.query?.uid;
-            console.log(uid);
             const options = { upsert: true };
             const filter = { uid: uid };
             const updateDoc = {
@@ -43,66 +31,29 @@ async function run() {
             }
             const result = await userCollections.updateOne(filter, updateDoc, options);
             res.send(result);
-<<<<<<< HEAD
         });
 
-        // insert google sign up
-        app.put('/users', async(req, res) =>{
-            const user = req.body;
-            const filter = await usersCollections.findOne(user);
-            if(!filter){
-                const result = await usersCollections.insertOne(user);
-                res.send(result);
-            }
-            else{
-                return;
-            }
-        });
-
-        // insert github sign up
-        app.put('/users', async(req, res) =>{
-            const user = req.body;
-            const filter = await usersCollections.findOne(user);
-            if(!filter){
-                const result = await usersCollections.insertOne(user);
-                res.send(result);
-            }
-            else{
-                return;
-            }
-        });
-
-        // get tutorial
-        app.get('/tutorial', async(req, res) =>{
-            const query = {};
-            const result = await tutorialCollections.find(query).toArray();
-            res.send(result);
-        });
-
-        // get categories
-        app.get('/categories', async(req, res) =>{
-            const query = {};
-            const result = await categoriesCollections.find(query).toArray();
-            res.send(result);
-=======
-        })
+        // all user get
         app.get('/user', async (req, res) => {
             const query = {};
             const users = await userCollections.find(query).toArray();
             res.send(users);
-        })
+        });
+
+        // post user 
         app.post('/userPost', async(req,res) =>{
             const userpost = req.body;
             const result = await userPostCollections.insertOne(userpost)
             console.log(result)
             res.send(result)
-        })
+        });
+
+        // get alluserpost
         app.get('/allUserPost', async(req,res) =>{
             const query = {}
             const result = await userPostCollections.find(query).toArray()
             res.send(result)
->>>>>>> main
-        })
+        });
 
     }
     finally {
